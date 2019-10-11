c27ef972ae07c2fb4549ef5673b2e9fe5228146e
Joshua Sansom <joshwebd@gmail.com>
2019-10-12 02:03:57 +1100

    REmoved some more unit tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,30 +5,30 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:rate_it/application/application.dart';
-import 'package:rate_it/state/state.dart';
-import 'package:redux/redux.dart';
-import 'package:flutter_redux/flutter_redux.dart';
+//import 'package:flutter/material.dart';
+//import 'package:flutter_test/flutter_test.dart';
+//import 'package:rate_it/application/application.dart';
+//import 'package:rate_it/state/state.dart';
+//import 'package:redux/redux.dart';
+//import 'package:flutter_redux/flutter_redux.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    var store = new Store<AppState>(appStateReducer, initialState: AppState.initialState());
-    await tester.pumpWidget(new StoreProvider(store: store, child: MyApp()));
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    var type = FloatingActionButton(onPressed: () => {},).runtimeType;
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byType(type));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+//  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//    // Build our app and trigger a frame.
+//    var store = new Store<AppState>(appStateReducer, initialState: AppState.initialState());
+//    await tester.pumpWidget(new StoreProvider(store: store, child: MyApp()));
+//
+//    // Verify that our counter starts at 0.
+//    expect(find.text('0'), findsOneWidget);
+//    expect(find.text('1'), findsNothing);
+//
+//    var type = FloatingActionButton(onPressed: () => {},).runtimeType;
+//    // Tap the '+' icon and trigger a frame.
+//    await tester.tap(find.byType(type));
+//    await tester.pump();
+//
+//    // Verify that our counter has incremented.
+//    expect(find.text('0'), findsNothing);
+//    expect(find.text('1'), findsOneWidget);
+//  });
 }
